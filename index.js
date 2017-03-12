3b7b0120cbbf911e7b363895ff87f56461460a1d
Oliver Leics <oliver.leics@gmail.com>
2017-03-12 12:46:26 +0100

    Implements Symbol and adds README.md

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+var withSymbol = typeof Symbol !== 'undefined';
+
 module.exports = isScalar;
 
 // null|undefined is considered to be scalar too
@@ -8,8 +10,10 @@ function isScalar(value) {
   if(type === 'string') return true;
   if(type === 'number') return true;
   if(type === 'boolean') return true;
+  if(withSymbol === true && type === 'symbol') return true;
 
   if(value == null) return true;
+  if(withSymbol === true && value instanceof Symbol) return true;
   if(value instanceof String) return true;
   if(value instanceof Number) return true;
   if(value instanceof Boolean) return true;
